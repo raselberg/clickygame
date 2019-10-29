df491241e32973b2e7122ae4b282aacea0162edf
pablo <raselberg@gmail.com>
2019-10-29 13:45:49 -0700

    Updates

---
+++ precache-manifest.7c40e018840e2d5cfba2b7c84c69f9f5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d23715c210d0d9401758422e8743ac5",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "b443de2449bdaf66d598",
+    "url": "/clickygame/static/css/main.25fc5909.chunk.css"
+  },
+  {
+    "revision": "348a6cff2b7c8ed5a195",
+    "url": "/clickygame/static/js/2.e44bafa4.chunk.js"
+  },
+  {
+    "revision": "b443de2449bdaf66d598",
+    "url": "/clickygame/static/js/main.a966b577.chunk.js"
+  },
+  {
+    "revision": "f19b085d24c116edb0ce",
+    "url": "/clickygame/static/js/runtime-main.b92a128d.js"
+  }
+]);
